06c4131ff97ad862e8ba4c21c5b4379398b63d99
Trung Nguyen Thien <ngthientrung@gmail.com>
2020-08-04 20:58:38 +0700

    no message

--- Sample/Sample/Sample/ViewController.swift
+++ Sample/Sample/Sample/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import UIBuilder
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let label = TextBuilder()
+            .fontSize(15)
+            .textColor(.red)
+            .label()
+        label.frame = CGRect(x: 0, y: 0, width: 200, height: 50)
+        label.text = "Hello world"
+        view.addSubview(label)
     }
 
 
