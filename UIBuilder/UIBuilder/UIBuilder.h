a45179376f07b4033ed22d87686adeda36ca6bd0
Trung Nguyen Thien <ngthientrung@gmail.com>
2020-10-07 23:03:54 +0700

    no message

---
+++ UIBuilder/UIBuilder/UIBuilder.h
@@ -0,0 +1,18 @@
+//
+//  UIBuilder.h
+//  UIBuilder
+//
+//  Created by Trung on 10/7/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UIBuilder.
+FOUNDATION_EXPORT double UIBuilderVersionNumber;
+
+//! Project version string for UIBuilder.
+FOUNDATION_EXPORT const unsigned char UIBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIBuilder/PublicHeader.h>
+
+
